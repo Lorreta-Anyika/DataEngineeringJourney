277fa1b828168ed645fede1132f9c5a6325dfe36
Anyika Uchechukwu Lorreta <u64007220@gmail.com>
2026-09-01 22:03:12 +0100

    creating variables

---
+++ Linux-git/welcome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Hello Data Engineers!"
+echo "This is my first Bash script running on $(hostname)"
+
+name="Lorreta"
+role="data engineer"
+
+echo "Hello, $name! Your role is $role."
+chmod +x welcome.sh
+
+
+
+
+
