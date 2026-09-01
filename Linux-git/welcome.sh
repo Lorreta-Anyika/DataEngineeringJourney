685d5b5e71bfbc2e048f36e7bdb4a646a4d9f721
Anyika Uchechukwu Lorreta <u64007220@gmail.com>
2026-09-01 22:45:12 +0100

    Learning Variables

--- Linux-git/welcome.sh
+++ Linux-git/welcome.sh
@@ -6,6 +6,36 @@ name="Lorreta"
 role="data engineer"
 
 echo "Hello, $name! Your role is $role."
+
+current_date=$(date)
+echo "The current date and time is: $current_date"
+
+#reading user input
+
+echo "Enter your project name:"
+read project
+echo "Creating project folder: $project"
+mkdir -p "/projects/$project"
+
+#Environment Variables: They are system defined already like $USER, $HOME, PWD, etc
+
+echo "I am logged in as a $USER on $HOSTNAME"
+
+#Exporting Variables: Used to make it available to child processes or other scripts
+
+export DATA_PATH="/data/raw"
+
+#FOR EXAMPLE
+
+echo $DATA_PATH
+
+#CONSTANTS and Read-only Variables
+#to achieve this we use the "readonly command
+
+readonly LOG_DIR="/var/logs/data"
+#now, the path for LOG_DIR cannot be overwritten. It remains constant. This is Useful when i dont want anybody on the team to ever change a path.
+LOG_DIR="tmp/logs" #This will cause an error
+
 chmod +x welcome.sh
 
 
