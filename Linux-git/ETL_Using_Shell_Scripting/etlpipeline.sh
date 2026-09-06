06242196e8495ab99b112f522740f5230f419f11
Anyika Uchechukwu Lorreta <u64007220@gmail.com>
2026-09-06 23:21:06 +0100

    transformation performed

--- Linux-git/ETL_Using_Shell_Scripting/etlpipeline.sh
+++ Linux-git/ETL_Using_Shell_Scripting/etlpipeline.sh
@@ -33,9 +33,46 @@ fi
 #TRANSFORMATION
 
 #Display top 5 rows with headers
-
 echo "The top 5 rows are displayed for manual reading"
-echo # a line break
+echo 
 head -n 5 raw/annual-enterprise-survey-2023.csv
 
+#create folder for transformed data
+Transformed="./Transformed"
+mkdir -p "Transformed"
 
+#Renaming of column
+INPUT_FILE="$raw/annual-enterprise-survey-2023.csv"
+OUTPUT_FILE="$Transformed/2023_year_finance.csv"
+OLD_COL="Variable_code"
+NEW_COL="variable_code"
+
+awk -v old="$OLD_COL" -v new="$NEW_COL" '
+NR == 1 {
+
+	#Rename the column specified
+	gsub(old, new)
+	
+
+	#select required columns using the idea of for loop; to avoid hardcoding things
+	for (i = 1; i<=NF; i++) {
+		if ($i == "Year") Year = i
+		if ($i == "Value") Value = i
+		if ($i == "Units") Units = i
+		if ($i == "variable_code") variable_code = i
+	}
+	
+	#print the new header
+	print "Year,Value,Units,variable_code"
+	
+	next
+}
+	#Select only the data from printed header
+{
+	print $Year "," $Value "," $Units "," $variable_code
+}
+
+' "$INPUT_FILE" > "$OUTPUT_FILE"
+
+echo "Transformation is completed."
+echo "Selected columns saved to: $OUTPUT_FILE"
